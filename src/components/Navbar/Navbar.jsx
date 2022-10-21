7de4aed402520363fdce9c89e634ff0fe257f2a1
shafeeq <sshafeeq4526@gmail.com>
2022-10-21 10:00:40 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Andrew</div>
+        <div className="n-name">Shafeeq</div>
         <Toggle />
       </div>
       {/* right */}
@@ -21,7 +21,7 @@ const navbar = () => {
             </li>
             <li>
               <Link to="services" spy={true} smooth={true}>
-                Serivces
+                Serivce
               </Link>
             </li>
             <li>
